f121d5c97f7fcaddc404e04a02aaf2ed79125380
Maik Tizziani <mtizziani@gmail.com>
2022-10-03 18:18:38 +0200

    embedded sample image names

--- src/Controller/Frontend/GalleryViewController.php
+++ src/Controller/Frontend/GalleryViewController.php
@@ -12,7 +12,16 @@ class GalleryViewController extends AbstractController
     public function index(): Response
     {
         return $this->render('frontend/gallery/view.html.twig',
-            []
+            [
+                '15-59-03-1214-Hochzeit-A&F.jpg',
+                '2022_07_30_Steinburg_Hochzeit_Mara_Sebastian_Foto_xtrakt_media_Lukas_Seufert-37-von-61.jpg',
+                'Feiern_Hochz5.jpg',
+                'hochzeit-2.jpg',
+                'hochzeit-4.jpg',
+                'hochzeit-auf-den-ersten-blick.jpg',
+                'Verlobung-bis-zur-Hochzeit.jpg',
+                'vonderpragmatischenehezurromantischentraumhochzeit100__v-ARDFotogalerie.jpg'
+            ]
         );
     }
 }
